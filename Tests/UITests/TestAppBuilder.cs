f21f2d4323449b48753e48bf5c2991c0b7798c55
Kalachik Roman <romankalachik@eremex.ru>
2025-07-16 17:43:17 +0300

    refactoring

---
+++ Tests/UITests/TestAppBuilder.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia;
+using Avalonia.Headless;
+using MortgageCalculator;
+
+[assembly: AvaloniaTestApplication(typeof(TestAppBuilder))]
+
+public class TestAppBuilder
+{
+    public static AppBuilder BuildAvaloniaApp() => AppBuilder.Configure<App>()
+        .UseHeadless(new AvaloniaHeadlessPlatformOptions());
+}
---
+++ Tests/UITests_E2E/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia.Controls;
+using MortgageCalculator.Views;
+
+namespace UITests;
+
+public class UnitTest1
+{
+    [Fact]
+    public async Task SimpleTest()
+    {
+        Window window = new MainWindow();
+        await window.ShowDialog(null);
+    }
+}
